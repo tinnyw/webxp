2141d0a1ba2a18bce0edbed43edd33fc9d0ba66f
root <root@Gizmo.>
2024-07-11 20:54:23 -0400

    start of websockets

--- src/lessons/peer/backend/server.go
+++ src/lessons/peer/backend/server.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/contrib/websocket"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
@@ -8,6 +11,10 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("LOGOS!!!!")
 	})
+
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		c.WriteJSON(fiber.Map{"msg": "LOGOS, i AGAPE YOU!!!! (from server)"})
+	}))
 
 	app.Listen(":8080")
 }
