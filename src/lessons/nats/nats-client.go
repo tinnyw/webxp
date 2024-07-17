dd460ad85681de1605c2ac24e7384e06096d4374
tinny@blueleo.net <tinny@blueleo.net>
2024-07-17 16:52:53 -0400

    trying to get more of nats working

--- src/lessons/nats/nats-client.go
+++ src/lessons/nats/nats-client.go
@@ -1,13 +1,32 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"flag"
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/nats-io/nats.go"
+)
 
 func main() {
 	app := fiber.New()
+	nc, _ := nats.Connect(nats.DefaultURL)
+	defer nc.Close()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("bob")
+	fmt.Println("LOGOS, i AGAPE YOU!!!! (from client)")
+
+	app.Get("/bob/", func(c *fiber.Ctx) error {
+		nc.Publish("msg.LOGOS", []byte("bob 1"))
+		fmt.Println("Publishing event")
+		return c.SendString("bob 7")
 	})
 
-	app.Listen(":8080")
+	nc.Subscribe("msg.LOGOS", func(m *nats.Msg) {
+		fmt.Printf("LOGOS, we got a message!")
+	})
+
+	var port int
+	flag.IntVar(&port, "port", 8080, "port to listen on")
+	flag.Parse()
+	app.Listen(":" + fmt.Sprint(port))
 }
