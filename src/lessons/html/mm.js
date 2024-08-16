3c790dea41fdc4f79481e122d425b6de0a41bbb3
tinny@blueleo.net <tinny@blueleo.net>
2024-08-15 21:41:17 -0400

    mm.js

---
+++ src/lessons/html/mm.js
@@ -0,0 +1,28 @@
+import * as THREE from 'three';
+
+const renderScene = () => {
+	const canvas = document.querySelector('#mm');
+	const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+
+	const fov = 75;
+	const aspect = 2;  // the canvas default
+	const near = 0.1;
+	const far = 5;
+	const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+	camera.position.z = 3;
+
+	const scene = new THREE.Scene();
+	const boxWidth = 3;
+	const boxHeight = 3;
+	const boxDepth = 3;
+	const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+	const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+
+	const cube = new THREE.Mesh(geometry, material);
+	scene.add(cube);
+
+	renderer.render(scene, camera);
+}
+
+renderScene()
